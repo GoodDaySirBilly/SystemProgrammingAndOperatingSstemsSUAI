e78533c58e2f139b2a6e05400c9ee0ad2e80dd5f
GoodDaySirBilly <vboxuser@vbox.myguest.virtualbox.org>
2025-05-12 14:19:52 +0300

    working script way to lab

--- signal_learning/tmp.c
+++ signal_learning/tmp.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
-#define __USE_POSIX
-#include <signal.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-
-void signal_handler(int signo){
-    fprintf(stdout, "\n%s catched!\n", "SIGINT");
-    exit(0);
-}
-
-
 int main(){
     
-    sigset_t set;
-    sigfillset(&set);
-    sigdelset(&set,SIGINT);
-    sigprocmask(SIG_BLOCK,&set,NULL);
+    
+    pid_t pid1,pid2,pid3,pid4,pid5,pid6;
 
-    if (signal (SIGINT, signal_handler) == SIG_ERR) {
-        fprintf (stderr, "Невозможно обработать SIGINT!\n");
-        exit (EXIT_FAILURE);
-    }
+    pid1 = fork();
 
-    while(1) pause();
+    if(pid1 == 0){
+
+        pid2 = fork();
+
+        if(pid2 == 0);
+        else if(pid2 > 0);
+        else perror("fork2");
+
+    }else if(pid1 > 0){
+
+        pid3 = fork();
+        
+        if(pid3 == 0){
+
+            pid4 = fork();
+
+            if(pid4 == 0){
+
+                pid5 = fork();
+
+                if(pid5 == 0);
+                else if(pid5 > 0){
+
+                    pid6 = fork();
+
+                    if(pid6 == 0);
+                    else if(pid6 > 0);
+
+                    else perror("fork6");
+
+                }else perror("fork5");
+
+            }else if(pid4 > 0);
+            else perror("fork4");
+
+        } else if(pid3 > 0){
+
+            char buff[100];
+            sprintf(buff,"%d", (int)getpid());
+            execl("/bin/pstree", "pstree", buff, "-p", NULL);
+        
+        }else perror("fork3");
+
+    }else perror("fork1");
+    
+    sleep(5);
+    
     return 0;
 }
